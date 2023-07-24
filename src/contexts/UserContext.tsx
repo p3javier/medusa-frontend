4bd3c15a5d69e34f54782dc3c4fb5bb51fe298d8
p3javier <javii.e.saverroes@gmail.com>
2023-07-24 21:26:36 +0200

    feat: added final changes to login, added DevBar for debugging porpuses while developing, added UserContext, updated yarn cache

---
+++ src/contexts/UserContext.tsx
@@ -0,0 +1,29 @@
+import { createContext, useEffect, useState, useContext } from 'react';
+
+import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
+
+import { app } from '../firebase';
+
+const auth = getAuth(app);
+export const UserContext = createContext<User | undefined>(undefined);
+
+export const useAuth = () => useContext(UserContext);
+
+const UserContextProvider = (props: any) => {
+  const [user, setUser] = useState<User>();
+  console.log('user in context', user);
+  useEffect(() => {
+    onAuthStateChanged(auth, (userState) => {
+      if (userState) {
+        setUser(userState);
+      } else {
+        setUser(undefined);
+      }
+    });
+  }, []);
+  return (
+    <UserContext.Provider value={user}>{props.children}</UserContext.Provider>
+  );
+};
+
+export default UserContextProvider;
--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -4,6 +4,8 @@ import { Footer } from './Footer';
 import { Header } from './Header';
 import { Meta } from '../layout/Meta';
 import { AppConfig } from '../utils/AppConfig';
+import UserContextProvider from '../contexts/UserContext';
+import DevBar from '../devBar/DevBar';
 
 type BaseProps = {
   children: ReactNode;
@@ -19,6 +21,11 @@ const Base = (props: BaseProps) => {
   return (
     <div className="antialiased text-gray-400 bg-slate-800 min-h-screen flex flex-col">
       <Meta title={metaTitle} description={description} />
+      {process.env.NODE_ENV === 'development' ? (
+        <UserContextProvider>
+          <DevBar />
+        </UserContextProvider>
+      ) : null}
       <Header />
       {children}
       <Footer />
