1bfd9f3c4738be979157437c72c7f4fb5bc25e2b
p3javier <javii.e.saverroes@gmail.com>
2023-09-06 00:51:09 +0200

    feat: added possibility of hide header in some pages that uses the Base template component

--- src/templates/Base.tsx
+++ src/templates/Base.tsx
@@ -10,18 +10,20 @@ type BaseProps = {
   children: ReactNode;
   metaTitle?: string;
   description?: string;
+  hideHeader?: boolean;
 };
 const Base = (props: BaseProps) => {
   const {
     children,
     metaTitle = AppConfig.title,
     description = AppConfig.description,
+    hideHeader = false,
   } = props;
   return (
     <div className="antialiased text-gray-400 bg-slate-800 min-h-screen flex flex-col">
       <Meta title={metaTitle} description={description} />
       {process.env.NODE_ENV === 'development' ? <DevBar /> : null}
-      <Header />
+      {hideHeader || <Header />}
       {children}
       <Footer />
     </div>
