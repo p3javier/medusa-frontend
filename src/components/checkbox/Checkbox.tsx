8e92fb35503ba2f8f3bf3ef8a288ab3a1153b577
p3javier <javii.e.saverroes@gmail.com>
2024-01-27 14:48:56 +0100

    feat: modified checkbox props

--- src/components/checkbox/Checkbox.tsx
+++ src/components/checkbox/Checkbox.tsx
@@ -1,17 +1,19 @@
 import { ChangeEventHandler } from 'react';
 
-type TextInputProps = {
+type CheckboxInputProps = {
+  readonly required?: boolean;
   readonly label: string;
   readonly name: string;
   onStatusChange: ChangeEventHandler<HTMLInputElement>;
 };
-const Checkbox = (props: TextInputProps) => (
+const Checkbox = (props: CheckboxInputProps) => (
   <div>
     <input
       onChange={props.onStatusChange}
       name={props.name}
       className="text-black mr-2 font-semibold indent-3 antialiased rounded-lg mt-2"
       type="checkbox"
+      required={props.required}
     />
     <label className="antialiased" htmlFor={props.name}>
       {props.label}
